192b7a9bd795a845deb2beb5e035ec900a5a1b55
Rick Wight <obpwilliams482@gmail.com>
2021-09-08 17:46:13 -0400

    attempted to put download links on the CSV page. WIP, not currently functional

---
+++ csv_page.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+	let html = '<b>Salary Data</b>' + '<br>' + '<br>' + '<a id="download-all-teams"> Download All Teams </a>' + '<br>' + '<br>';
+
+	$words = $('<div>My Div!</div>')
+
+	$(linkSet).insertBefore('table[bgcolor="black"]');
+});
